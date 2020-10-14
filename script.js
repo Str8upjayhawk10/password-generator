2e96d5355f3a4924c30c60de72fc885de932a062
Nathaniel Meadows <str8upjayhawk10@gmail.com>
2020-10-14 16:48:00 -0700

    error password generator undefined

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-// Generate function for writepassword
-function writePassword(){
-    let password = getPasswordOption();
-    let passwordText = document.querySelector('#password');
-
-    passwordText.value = password;
-}
-
-
 // Password string format
 let num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 let lower =  ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
